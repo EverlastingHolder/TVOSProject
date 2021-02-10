e89d672ac50af42f66278f816981f89a227c6df4
EverlastingHolder <moshkovcevroma@gmail.com>
2021-02-10 15:55:09 +0700

    Initial Commit

---
+++ TVTestProject/TVTestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TVTestProjectApp.swift
+//  TVTestProject
+//
+//  Created by admin on 10.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct TVTestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
